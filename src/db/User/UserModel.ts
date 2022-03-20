819db963f483646cbf9429529bcd6702c6317280
Milan Brkic <milan.brkic1998@gmail.com>
2022-03-20 14:51:10 +0100

    unique image only mint

--- src/db/User/UserModel.ts
+++ src/db/User/UserModel.ts
@@ -19,6 +19,11 @@ class UserModel{
         return user;
     }
 
+    public async getByImage(imageHash:string): Promise<User | null>{
+        const user =  (await this.model.find({imagesHash:{$in:imageHash}}).exec())[0];
+        return user;
+    }
+
     public async insert(address:string):Promise<User | null>{
         return this.model.create({address});
     }
@@ -26,6 +31,8 @@ class UserModel{
     public async addAnImage(address:string, imageHash: string): Promise<User | null>{
         return this.model.updateOne({address}, {$push:{imagesHash: imageHash}})
     }
+
+
 }
 
 const userModel = new UserModel();
--- src/endpoints/mint.ts
+++ src/endpoints/mint.ts
@@ -2,13 +2,21 @@ import MulterRequest from "../multer/MulterRequest";
 import {Response} from "express";
 import crypto from "crypto";
 import userModel from "../db/User/UserModel";
+import User from "../db/User/User";
 
 
 export default async function mint(req: MulterRequest, res: Response) {
     const hash = crypto.createHash('md5').update(req.file.buffer).digest('hex');
-    const address  = req.body.account;
-    console.log(`hash ${hash} user ${address}`)
-    await userModel.addAnImage(address, hash);
-
-    res.status(200).send("Hola Mundo");
+    const user: User = await userModel.getByImage(hash);
+    
+    if(!user){
+      const address  = req.body.account;
+      await userModel.addAnImage(address, hash);
+      console.log(`Image ${hash} minted | Address ${address}`)
+      res.status(200).send("Hola Mundo");
+    }
+    else{
+      console.log(`Image ${hash} already minted minted`)
+      res.status(404).send("Already minted");
+    }
   }
